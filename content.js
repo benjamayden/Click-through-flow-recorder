75dc5cce92b5bfba1040bde3ff6e9ccd90500c76
benjamayden <145757642+benjamayden@users.noreply.github.com>
2024-11-08 11:58:52 +0000

    permission fixed

--- content.js
+++ content.js
@@ -26,25 +26,6 @@ if (!window.hasContentScriptRun) {
         });
     }
 
-    // Function to show toast messages
-    function showToast(message) {
-        const toast = document.createElement('div');
-        toast.className = 'toast';
-        toast.textContent = message;
-        document.body.appendChild(toast);
-
-        setTimeout(() => {
-            toast.classList.add('show');
-        }, 100);
-
-        setTimeout(() => {
-            toast.classList.remove('show');
-            setTimeout(() => {
-                toast.remove();
-            }, 500);
-        }, 3000);
-    }
-
     // Listener for `isRecording` changes
     chrome.storage.onChanged.addListener((changes, area) => {
         if (area === 'local' && changes.isRecording) {
